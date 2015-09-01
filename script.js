d37e855920208eb16c3875beae0018b4958e607c
Sanomi <snrutledge@gmail.com>
2015-09-01 04:41:12 -0700

    almost everything working. just need to not allow two selections

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ function init() {
 	$('#block1').val(1);
 	$('#block2').val(2);	
 	$('#block3').val(3);
-	var blockSelected = $('.selected').length;	
 	$('.block').on('click', blockToggle);
 	$('.tower').on('click', moveBlock);
+	$('.tower').on('click', test);
 }
 
 function blockToggle(event){
 	event.stopPropagation();
 	var $this = $(this);
 	if ($( this ).is( ":first-child" )) {
+		console.log($('.selected').length === 0);
 		if ($(this).hasClass("selected")) {
 			$(this).removeClass('selected').addClass('unselected');
+			console.log($('.selected').length === 0);
 		} else {
 		$(this).removeClass('unselected').addClass('selected');
 			}
@@ -29,9 +31,6 @@ function moveBlock(event) {
 	var $this = $(this);
 	var $wrapper = $this.children();
 	var $selectedBlock = $('.selected');
-		// if ($selectedBlock.length === 0) {
-			console.log($wrapper.children(':first-child').val());
-			console.log($selectedBlock.val());
 			if ($wrapper.children().length === 0 || $wrapper.children(':first-child').val() > $selectedBlock.val()) {
 				var $originTower = $('.selected').parent().parent().attr('id');
 				var $destinationTower = $this.attr('id');
@@ -41,5 +40,11 @@ function moveBlock(event) {
 					$('.selected').addClass('unselected').removeClass('selected');
 				}
 			}
-		// }
 	};
+
+	function test() {
+			console.log($('#tower3').children().children().length);
+	if ($('#tower3').children().children().length === 3) {
+		alert("You won. No one cares.\n Tower games are for sadists\nGo play outside. Now");
+	}
+}
