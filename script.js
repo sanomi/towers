0c94db9d343610cba84a3f62e39c934b9eafc745
Sanomi <snrutledge@gmail.com>
2015-09-01 04:08:07 -0700

    everything working, needs winner code

--- script.js
+++ script.js
@@ -3,38 +3,43 @@
 $(document).ready(init);
 
 function init() {
-	console.log('this');
 	$('#block1').val(1);
 	$('#block2').val(2);	
 	$('#block3').val(3);
 	var blockSelected = $('.selected').length;	
-	console.log(blockSelected);
 	$('.block').on('click', blockToggle);
 	$('.tower').on('click', moveBlock);
 }
 
 function blockToggle(event){
-	console.log('block');
 	event.stopPropagation();
-	if ($(this).hasClass("selected")) {
-		$(this).removeClass('selected').addClass('unselected');
-	} else {
-	$(this).removeClass('unselected').addClass('selected');
-		}
+	var $this = $(this);
+	if ($( this ).is( ":first-child" )) {
+		if ($(this).hasClass("selected")) {
+			$(this).removeClass('selected').addClass('unselected');
+		} else {
+		$(this).removeClass('unselected').addClass('selected');
+			}
+	}
 };
 
 
 function moveBlock(event) {
 	event.stopPropagation();
 	var $this = $(this);
-	var $selectedBlock = $this.children().children('.selected')
-		if ($selectedBlock.length === 0) {
-			var $originTower = $('.selected').parent().parent().attr('id');
-			var $destinationTower = $this.attr('id');
-			if ($originTower !== $destinationTower) {
-				var selectedBox = $('.selected');
-				$this.children().prepend(selectedBox);
-				$('.selected').addClass('unselected').removeClass('selected');
+	var $wrapper = $this.children();
+	var $selectedBlock = $('.selected');
+		// if ($selectedBlock.length === 0) {
+			console.log($wrapper.children(':first-child').val());
+			console.log($selectedBlock.val());
+			if ($wrapper.children().length === 0 || $wrapper.children(':first-child').val() > $selectedBlock.val()) {
+				var $originTower = $('.selected').parent().parent().attr('id');
+				var $destinationTower = $this.attr('id');
+				if ($originTower !== $destinationTower) {
+					var selectedBox = $('.selected');
+					$this.children().prepend(selectedBox);
+					$('.selected').addClass('unselected').removeClass('selected');
+				}
 			}
-		}
+		// }
 	};
